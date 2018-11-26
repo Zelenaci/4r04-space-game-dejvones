b41790cfe6880e645e29355ad3f21325ecb5e2db
David Vlasák <31956784+dejvones@users.noreply.github.com>
2018-11-26 20:45:14 +0100

    No, nevím nevím

--- kameny.py
+++ kameny.py
@@ -9,37 +9,28 @@ import random
 from math import sin, cos, radians, pi
 from pyglet.window import key
 
-
-window = pyglet.window.Window(1000, 800)
-batch = pyglet.graphics.Batch()   # pro optimalizované vyreslování objektů
+window = pyglet.window.Window(1000, 600)
+batch = pyglet.graphics.Batch()
 klavesy = set ()
 
 class Stone(object):
 
-    def __init__(self, x=None, y=None, direction=None, speed=None, rspeed=None):
-        # nečtu obrázek
-        num = random.choice(range(0, 20))
-        self.image = pyglet.image.load('meteors/{}.png'.format(num))
-        # střed otáčení dám na střed obrázku
+    def __init__(self):
+        num = random.choice(range(1, 20))
+        
+        self.image = pyglet.image.load('SpaceShooterRedux/PNG/meteors/{}.png'.format(num))
         self.image.anchor_x = self.image.width // 2
         self.image.anchor_y = self.image.height // 2
-        # z obrázku vytvořím sprite
         self.sprite = pyglet.sprite.Sprite(self.image, batch=batch)
-        # pokud není atribut zadán vytvořím ho náhodně
-        self.x = x if x is not None else random.randint(0,1000)
-        self.y = y if y is not None else random.randint(200,800)
-        # musím správně nastavit polohu sprite
+        self.x =random.randint(0,1000)
+        self.y =random.randint(200,800)
         self.sprite.x = self.x
         self.sprite.y = self.y
-        self.direction = direction \
-            if direction is not None else random.randint(0, 359)
-        # rychlost pohybu
-        self.speed = speed \
-            if speed is not None else random.randint(80, 120)
-        # rychlost otáčení
-        self.rspeed = rspeed \
-            if rspeed is not None else random.randint(-100, 100)
+        self.direction =random.randint(0, 359)
+        self.speed = random.randint(40, 60)
+        self.rspeed = random.randint(-100, 100)
         self.rotation = 0
+        self.rozmer = min(self.image.width, self.image.height)/2
 
     def tick(self, dt):
         self.bounce()
@@ -49,27 +40,30 @@ class Stone(object):
         self.y += dt * self.speed * sin(pi / 2 - radians(self.direction))
         self.sprite.y = self.y
         self.sprite.rotation += 0.01 * self.rspeed
+        
+        if abs(self.x-raketa.x)< self.image.width / 2 and abs(self.y-raketa.y) < self.image.height /2:
+            print('Prohra')
+        else:
+            pass
+
     def bounce(self):
-        # vzdálenost okraje a středu
-        rozmer = min(self.image.width, self.image.height)/2
-        if self.x + rozmer >= window.width:
+        if self.x + self.rozmer >= window.width:
             self.direction = random.randint(190, 350)
             return
-        if self.x - rozmer <= 0:
+        if self.x - self.rozmer <= 0:
             self.direction = random.randint(10, 170)
             return
-        if self.y + rozmer >= window.height:
+        if self.y + self.rozmer >= window.height:
             self.direction = random.randint(100, 260)
             return
-        if self.y - rozmer <= 0:
+        if self.y - self.rozmer <= 0:
             self.direction = random.randint(-80, 80)
             return
 
 class Raketa(object):
+
         def __init__(self, x=None, y=None, direction=None, speed=None, rspeed=None):
-            global smer
-            global rychlost
-            self.image = pyglet.image.load('playerShip1_red.png')
+            self.image = pyglet.image.load('SpaceShooterRedux/PNG/playerShip1_red.png')
             # střed otáčení dám na střed obrázku
             self.image.anchor_x = self.image.width // 2
             self.image.anchor_y = self.image.height // 2
@@ -81,61 +75,51 @@ class Raketa(object):
             self.sprite.x = self.x
             self.sprite.y = self.y       
             self.rychlost = 0
-            self.uhel = 0  
-            
+            self.uhel = 0
+            self.rozmer = min(self.image.width, self.image.height)/2
+
         def tick(self, dt):
-            self.bounce()
-            rozmer = min(self.image.width, self.image.height)/2
+            
             for sym in klavesy:   
                 if sym == key.RIGHT:
                     self.rotation += 10
                     self.uhel += 10
+                    
                 elif sym == key.LEFT:
                     self.rotation -= 10
                     self.uhel -= 10
+                    
                 elif sym == key.UP:
                     self.rychlost = 200
                     self.x += dt * self.rychlost * cos(pi / 2 - radians(self.uhel))
-                    if self.x + rozmer >= window.width or self.x - rozmer <= 0:
-                        pass
+                    if self.x + self.rozmer >= window.width or self.x - self.rozmer <= 0:
+                        self.x = self.sprite.x
                     else:    
                         self.sprite.x = self.x
                     self.y += dt * self.rychlost * sin(pi / 2 - radians(self.uhel))
-                    if self.y + rozmer >= window.height or self.y - rozmer <= 0:
-                        pass
+                    if self.y + self.rozmer >= window.height or self.y - self.rozmer <= 0:
+                        self.y = self.sprite.y
                     else:
-                        self.sprite.y = self.y   
+                        self.sprite.y = self.y  
+                        
                 elif sym == key.DOWN:
                     self.rychlost = 200
                     self.x += dt * self.rychlost * -cos(pi / 2 - radians(self.uhel))
-                    if self.x + rozmer >= window.width or self.x - rozmer <= 0:
-                        pass
+                    if self.x + self.rozmer >= window.width or self.x - self.rozmer <= 0:
+                        self.x = self.sprite.x
                     else:    
                         self.sprite.x = self.x
                     self.y += dt * self.rychlost * -sin(pi / 2 - radians(self.uhel))
-                    if self.y + rozmer >= window.height or self.y - rozmer <= 0:
-                        pass
+                    if self.y + self.rozmer >= window.height or self.y - self.rozmer <= 0:
+                        self.y = self.sprite.y
                     else:
                         self.sprite.y = self.y 
+                        
             self.sprite.rotation = self.rotation
 
-        def bounce(self):
-            rozmer = min(self.image.width, self.image.height)/2
-            if self.x + rozmer >= window.width:
-                self.rychlost = 0
-                return
-            if self.x - rozmer <= 0:
-                self.rychlost = 0
-                return
-            if self.y + rozmer >= window.height:
-                self.rychlost = 0
-                return
-            if self.y - rozmer <= 0:
-                self.rychlost = 0
-                return
 
 stones = []
-for i in range(30):
+for i in range(15):
     stone = Stone()
     pyglet.clock.schedule_interval(stone.tick, 1 / 60)
     stones.append(stone)
